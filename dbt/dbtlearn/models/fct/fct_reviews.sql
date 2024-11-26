073b8066c823d43415b23f1a1bb7881bfbe9058d
Phi Nguyen Laptop <phinguyen@laptop>
2024-11-26 16:04:04 +0700

    dbt learning added

---
+++ dbt/dbtlearn/models/fct/fct_reviews.sql
@@ -0,0 +1,16 @@
+-- model level materialization
+-- incremental materialization will fail if the schema of the source changed
+{{
+  config(
+    materialized = 'incremental',
+    on_schema_change='fail'
+    )
+}}
+with src_reviews as (
+    select * from {{ ref("src_reviews") }}
+)
+select * from src_reviews
+where review_text is not null
+{% if is_incremental() %}
+  and review_date > (select max(review_date) from {{ this }}) -- this refer to this model - fct_reviews
+{% endif %}
